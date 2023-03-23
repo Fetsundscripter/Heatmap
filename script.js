3dcb24609eda6d51e07fbc3c49e733b637d74562
Fetsundscripter <jens.petter.haraldsen@gmail.com>
2023-03-23 21:35:15 +0100

    focused on getting the code clean this round but still feel like im kind of stubbuling. trying to understand why what im doing is working.

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
 let req = new XMLHttpRequest()
 
 let baseTemp
-let values
+let values = []
 
 let xScale
 let yScale
 
+let minYear
+let maxYear
+
 let width = 1200
 let height = 600
-let padsding = 60
+let padding = 60
 
 let canvas = d3.select('#canvas')
 canvas.attr('width', width)
@@ -17,12 +20,82 @@ canvas.attr('height', height)
 
 let generateScales = () => {
 
+    let minYear = d3.min(values, (item) =>{
+        return item['year']
+    })
+
+    maxYear = d3.max(values, (item) =>{
+        return item['year']
+    })
+
+    xScale = d3.scaleLinear()
+               .domain([minYear, maxYear])
+               .range([padding, width - padding])
+
+    yScale = d3.scaleTime()
+               .domain([new Date(0, 0, 0, 0, 0, 0, 0, ), new Date(0, 12, 0, 0, 0, 0, 0)])
+               .range([padding, height - padding])
 }
 
 let drawCells = () => {
 
+    canvas.selectAll('rect')
+          .data(values)
+          .enter()
+          .append('rect')
+          .attr('class', 'cell')
+          .attr('fill', (item) => {
+            variance = item['variance']
+            if(variance <= -1){
+                return 'SteelBlue'
+            } else if (variance <= 0){
+                return 'LightSteelBlue'
+            } else if (variance <= 1){
+                return 'Orange'
+            } else {
+                return 'Crimson'
+            }
+          })
+          .attr('data-year', (item) => {
+            return item['year']
+          })
+          .attr('data-month', (item) => {
+            return item['month'] - 1
+          })
+          .attr('data-temp', (item) =>{
+            return baseTemp + item['variance']
+          })
+          .attr('height', (height - (2 * padding)) / 12)
+          .attr('y', (item) =>{
+            return yScale(new Date(0, item['month'] -1, 0, 0, 0, 0, 0))
+          })
 }
 
 let drawAxes = () => {
-    
+
+    let xAxis = d3.axisBottom(xScale)
+    let yAxis = d3.axisLeft(yScale)
+
+    canvas.append('g')
+          .call(xAxis)
+          .attr('id', 'x-axis')
+          .attr('transform', 'translate(0,' + (height-padding) + ')')
+
+    canvas.append('g')
+          .call(yAxis)
+          .attr('id', 'y-axis')
+          .attr('transform', 'translate(' + padding + ',0)')
 }
+
+req.open('GET', url, true)
+req.onload = () => {
+    let object = JSON.parse(req.responseText)
+    baseTemp = object['baseTemperature']
+    values = object['monthlyVariance']
+    console.log(baseTemp)
+    console.log(values)
+    generateScales()
+    drawCells()
+    drawAxes()
+}
+req.send()
